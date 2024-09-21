aa77a47f491f8ebd1d3d0a066a25ff86fad1161a
Thang <21130535@st.hcmuaf.edu.vn>
2024-09-21 19:15:02 +0700

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,35 +1,31 @@
-import { authMiddleware, clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-// const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
 
-// export default clerkMiddleware((auth, request) => {
-//   if (!isPublicRoute(request)) {
-//     auth().protect()
-//   }
-// })
-export default authMiddleware({
-  publicRoutes:["/api/:path*"]
-})
-export function middleware(req: Request) {
+export default clerkMiddleware((auth, request) => {
+  // CORS settings
   const res = NextResponse.next();
 
-  res.headers.set("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.headers.set("Access-Control-Allow-Origin", "http://localhost:3000"); // Adjust this for your production domain
   res.headers.set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
   res.headers.set("Access-Control-Allow-Headers", "Content-Type");
 
+  if (!isPublicRoute(request)) {
+    auth().protect();
+  }
+
   // Handle preflight requests
-  if (req.method === "OPTIONS") {
+  if (request.method === "OPTIONS") {
     return new NextResponse(null, { status: 204, headers: res.headers });
   }
 
   return res;
-}
+});
+
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 };
